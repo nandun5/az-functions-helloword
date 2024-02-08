940ead5d58da52f06cd3536cbbc7e7aeaf9ae6fe
Nandun Wijesinghe <nandunwijesinghe@bloominbrands.com>
2024-02-08 16:49:48 -0500

    Updating framework

--- hello-world.cs
+++ hello-world.cs
@@ -1,12 +1,11 @@
-using System;
 using System.IO;
+using System.Text.Json;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
-using Newtonsoft.Json;
 
 namespace Nandun.Functions
 {
@@ -22,7 +21,8 @@ namespace Nandun.Functions
             string name = req.Query["name"];
 
             string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            dynamic data = JsonConvert.DeserializeObject(requestBody);
+            dynamic data = JsonSerializer.Deserialize<dynamic>(requestBody);
+
             name = name ?? data?.name;
 
             return name != null
